94a95ad60d02563a1e568cc96594f4999fe79d7f
t-knapp <tobias-knapp@web.de>
2018-07-09 22:04:12 +0200

    Changed to node js environment

--- server/src/main/java/de/fhbingen/binhungrig/server/Utils.java
+++
@@ -1,9 +0,0 @@
-package de.fhbingen.binhungrig.server;
-
-public class Utils {
-
-	public static void log(final String format, final Object... args){
-		System.out.println(String.format(format, args));
-	}
-	
-}
